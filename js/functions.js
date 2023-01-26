44e93f9c520aa3ef9e1a94d10a0218efaf4bb302
Ali Seifi <seifiali84@gmail.com>
2023-01-26 22:51:55 +0330

    functions completed.

--- js/functions.js
+++ js/functions.js
@@ -3,9 +3,8 @@ var myVar = setInterval(function () {
     myTimer();
 }, 1000);
 function myTimer() {
-    var d = new Date();
-    document.getElementById("clock").innerHTML = d.toLocaleTimeString();
-    document.getElementById("date").innerHTML = ConvertSecToDate();
+    document.getElementById("clock").innerText = GetClock();
+    document.getElementById("date").innerText = GetDate();
 }
 console.log("functions.js ended.");
 
@@ -34,3 +33,27 @@ function ConvertSecToDate() // its just a test function
     return "years : " + year + " | months : " + month + " | days : " + day + " | hours : " + hour + " | minutes : " + min + " | seconds : " + sec ;
 }
 console.log(ConvertSecToDate());
+
+function GetClock(){
+    var sec = GetPassedSec();
+    var min = Math.floor(sec / 60);
+    var hour = Math.floor(min / 60);
+    var day = Math.floor(hour / 24);
+    sec = sec - min * 60;
+    min = min - hour * 60;
+    hour = hour - day * 24;
+    return hour + ":" + min + ":" + sec;
+}
+console.log(GetClock());
+function GetDate(){
+    var sec = GetPassedSec();
+    var min = Math.floor(sec / 60);
+    var hour = Math.floor(min / 60);
+    var day = Math.floor(hour / 24);
+    var month = Math.floor(day / 30);
+    var year = Math.floor(month / 12);
+    day = day - month * 30;
+    month = month - year * 12;
+    return year + "/" + month + "/" + day;
+}
+console.log(GetDate());
